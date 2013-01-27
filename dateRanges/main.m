cec4155f289c3230b045503a99d6877db4f02ccd
John Nye <john@johnnye.net>
2013-01-27 18:57:44 +0000

    Initial Commit

---
+++ dateRanges/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  dateRanges
+//
+//  Created by John Nye on 27/01/2013.
+//  Copyright (c) 2013 NimbleWorks LLP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
